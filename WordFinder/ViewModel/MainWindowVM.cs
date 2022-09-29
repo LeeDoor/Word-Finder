c24ec20b105558c5051333e83ec52130b6c9213d
Leonid Samoshenko <86494836+LeeDoor@users.noreply.github.com>
2022-09-29 17:11:54 +0300

    view model

---
+++ WordFinder/ViewModel/MainWindowVM.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism;
+using Prism.Commands;
+using WordFinder.Model;
+
+namespace WordFinder.ViewModel
+{
+    public class MainWindowVM : INotifyPropertyChanged
+    {
+        private string forbiddenWords = "split with 1 space";
+        private string initFolder = "";
+        private int maxProgressBar;
+        private int valProgressBar;
+
+        public string ForbiddenWords 
+        {
+            get => forbiddenWords;
+            set
+            {
+                ForbiddenWords = value;
+                OnPropertyChanged(nameof(ForbiddenWords));
+            }
+        }
+        public string InitFolder
+        {
+            get => initFolder;
+            set
+            {
+                initFolder = value;
+                OnPropertyChanged(nameof(InitFolder));
+            }
+        }
+        public int MaxProgressBar
+        {
+            get => maxProgressBar;
+            set
+            {
+                maxProgressBar = value;
+                OnPropertyChanged(nameof(MaxProgressBar));
+            }
+        }
+        public int ValProgressBar
+        {
+            get => valProgressBar;
+            set
+            {
+                valProgressBar = value;
+                OnPropertyChanged(nameof(ValProgressBar));
+            }
+        }
+
+        public DelegateCommand StartCommand { get; set; }
+
+        public MainWindowVM()
+        {
+            StartCommand = new DelegateCommand(FoldersReader.call);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
