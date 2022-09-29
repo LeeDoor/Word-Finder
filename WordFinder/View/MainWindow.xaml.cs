057662813574188956bb0abaa7b78caed534e09e
Leonid Samoshenko <86494836+LeeDoor@users.noreply.github.com>
2022-09-29 16:40:00 +0300

    folder creating, window moving

--- WordFinder/View/MainWindow.xaml.cs
+++ WordFinder/View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WordFinder
+namespace WordFinder.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
